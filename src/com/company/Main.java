96f30860c73bb1545a3b62a427f9463984ad7f9b
Flare1337 <dimonhek1234567@mail.ru>
2021-11-04 21:27:51 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        var name = "User: " + System.getProperty("user.name");
+        System.out.println(name);
+    }
+}
